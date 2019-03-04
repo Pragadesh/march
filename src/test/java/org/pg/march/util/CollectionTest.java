ba5a50449e369f235c01ccd0706b4e8b8dec6c59
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-03-03 18:35:06 -0800

    test collection methods

---
+++ src/test/java/org/pg/march/util/CollectionTest.java
@@ -0,0 +1,14 @@
+package org.pg.march.util;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class CollectionTest {
+
+	@Test
+	public void testGenericAssignment() {
+		List<String> list = Collections.emptyList();
+	}
+}
