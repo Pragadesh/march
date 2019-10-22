1cbe045fffa9fd4743e60fda1a97ffdc282b77a5
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-10-22 11:14:31 -0700

    Job scheduling for max profit

--- src/main/java/org/pg/march/lc/graph/MaximumProfitJobScheduling.java
+++ src/main/java/org/pg/march/lc/graph/MaximumProfitJobScheduling.java
@@ -1,11 +1,74 @@
 package org.pg.march.lc.graph;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /*
- * https://leetcode.com/contest/weekly-contest-159/problems/maximum-profit-in-job-scheduling/
+ * https://leetcode.com/problems/maximum-profit-in-job-scheduling/
  */
 public class MaximumProfitJobScheduling {
 
 	public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-		return 0;
+		PriorityQueue<IntervalNode> startDatePQ = new PriorityQueue<>(Comparator.comparingInt(IntervalNode::getStart));
+		for(int i=0; i<startTime.length; i++) {
+			IntervalNode node = new IntervalNode(startTime[i], endTime[i], profit[i]);
+			startDatePQ.offer(node);
+		}
+		
+		PriorityQueue<IntervalNode> endDatePQ = new PriorityQueue<>(Comparator.comparingInt(IntervalNode::getEnd));
+		while(!startDatePQ.isEmpty()) {
+			IntervalNode newStartNode = startDatePQ.poll();
+			if(endDatePQ.isEmpty() || endDatePQ.peek().end > newStartNode.start) {
+				endDatePQ.offer(newStartNode);
+			}
+			List<IntervalNode> nodesToAddBack = new ArrayList<>();
+			while(!endDatePQ.isEmpty() && endDatePQ.peek().end <= newStartNode.start) {
+				IntervalNode endNode = endDatePQ.poll();
+				nodesToAddBack.add(endNode);
+				IntervalNode newNode = new IntervalNode(newStartNode.start, newStartNode.end, newStartNode.profit + endNode.profit);
+				endDatePQ.offer(newNode);
+			}
+			endDatePQ.addAll(nodesToAddBack);
+		}
+		
+		int maxProfit = 0;
+		while(!endDatePQ.isEmpty()) {
+			maxProfit = Math.max(maxProfit, endDatePQ.poll().getProfit());
+		}
+		
+		return maxProfit;
+	}
+	
+	
+	private static class IntervalNode{
+		private final int start;
+		private final int end;
+		private int profit;
+		
+		public IntervalNode(int start, int end, int profit) {
+			this.start = start;
+			this.end = end;
+			this.profit = profit;
+		}
+
+		public int getStart() {
+			return start;
+		}
+
+		public int getEnd() {
+			return end;
+		}
+
+		public int getProfit() {
+			return profit;
+		}
+
+		@Override
+		public String toString() {
+			return "[start=" + start + ", end=" + end + ", profit=" + profit + "]";
+		}
+
 	}
 }
--- src/test/java/org/pg/march/lc/graph/MaximumProfitJobSchedulingTest.java
+++ src/test/java/org/pg/march/lc/graph/MaximumProfitJobSchedulingTest.java
@@ -27,7 +27,7 @@ public class MaximumProfitJobSchedulingTest {
 		int[] startTime = { 1, 2, 3, 4, 6 };
 		int[] endTime = { 3, 5, 10, 6, 9 };
 		int[] profit = { 20, 20, 100, 70, 60 };
-		Assert.assertEquals(120, maximumProfitJobScheduling.jobScheduling(startTime, endTime, profit));
+		Assert.assertEquals(150, maximumProfitJobScheduling.jobScheduling(startTime, endTime, profit));
 	}
 
 	@Test
@@ -35,6 +35,6 @@ public class MaximumProfitJobSchedulingTest {
 		int[] startTime = { 1, 1, 1 };
 		int[] endTime = { 2, 3, 4 };
 		int[] profit = { 5, 6, 4 };
-		Assert.assertEquals(120, maximumProfitJobScheduling.jobScheduling(startTime, endTime, profit));
+		Assert.assertEquals(6, maximumProfitJobScheduling.jobScheduling(startTime, endTime, profit));
 	}
 }
